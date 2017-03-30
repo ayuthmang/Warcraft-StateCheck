8e03d4c2856c9425fe23c72116e3e3a8adb5d66f
blackSource <blackSource@windowslive.com>
2017-03-31 00:00:02 +0700

    Added lastest project

---
+++ Warcraft StateCheck/frm_Main.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_Main))
+        Me.ToolTip_Info = New System.Windows.Forms.ToolTip(Me.components)
+        Me.tmr_CheckAvalibleProcess = New System.Windows.Forms.Timer(Me.components)
+        Me.tmr_ReadMemory = New System.Windows.Forms.Timer(Me.components)
+        Me.tmr_ReadMemory2 = New System.Windows.Forms.Timer(Me.components)
+        Me.tmr_AddText = New System.Windows.Forms.Timer(Me.components)
+        Me.ThemeMain = New Warcraft_StateCheck.PositronTheme()
+        Me.PositronDivider1 = New Warcraft_StateCheck.PositronDivider()
+        Me.btn_Enable = New Warcraft_StateCheck.PositronButton()
+        Me.btn_Minimize = New Warcraft_StateCheck.PositronButton()
+        Me.btn_Close = New Warcraft_StateCheck.PositronButton()
+        Me.lb_State = New Warcraft_StateCheck.PositronLabel()
+        Me.lb_Status = New Warcraft_StateCheck.PositronLabel()
+        Me.rdb_Soundplayer = New Warcraft_StateCheck.PositronRadioButton()
+        Me.rdb_Msgbox = New Warcraft_StateCheck.PositronRadioButton()
+        Me.rdb_Autofocus = New Warcraft_StateCheck.PositronRadioButton()
+        Me.ThemeMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolTip_Info
+        '
+        Me.ToolTip_Info.ToolTipTitle = "Info"
+        '
+        'tmr_CheckAvalibleProcess
+        '
+        '
+        'tmr_ReadMemory
+        '
+        '
+        'tmr_ReadMemory2
+        '
+        '
+        'tmr_AddText
+        '
+        Me.tmr_AddText.Interval = 250
+        '
+        'ThemeMain
+        '
+        Me.ThemeMain.BackColor = System.Drawing.Color.FromArgb(CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer))
+        Me.ThemeMain.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.ThemeMain.Controls.Add(Me.PositronDivider1)
+        Me.ThemeMain.Controls.Add(Me.btn_Enable)
+        Me.ThemeMain.Controls.Add(Me.btn_Minimize)
+        Me.ThemeMain.Controls.Add(Me.btn_Close)
+        Me.ThemeMain.Controls.Add(Me.lb_State)
+        Me.ThemeMain.Controls.Add(Me.lb_Status)
+        Me.ThemeMain.Controls.Add(Me.rdb_Soundplayer)
+        Me.ThemeMain.Controls.Add(Me.rdb_Msgbox)
+        Me.ThemeMain.Controls.Add(Me.rdb_Autofocus)
+        Me.ThemeMain.Customization = "0NDQ/2RkZP8AAAD/0tLS/9zc3P/IyMj/lpaW/w=="
+        Me.ThemeMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ThemeMain.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.ThemeMain.Image = Nothing
+        Me.ThemeMain.Location = New System.Drawing.Point(0, 0)
+        Me.ThemeMain.Movable = True
+        Me.ThemeMain.Name = "ThemeMain"
+        Me.ThemeMain.NoRounding = False
+        Me.ThemeMain.Sizable = False
+        Me.ThemeMain.Size = New System.Drawing.Size(290, 164)
+        Me.ThemeMain.SmartBounds = True
+        Me.ThemeMain.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.ThemeMain.TabIndex = 0
+        Me.ThemeMain.Text = "Warcraft State Check 1.26a for DotA"
+        Me.ThemeMain.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ThemeMain.Transparent = False
+        '
+        'PositronDivider1
+        '
+        Me.PositronDivider1.BackColor = System.Drawing.Color.Transparent
+        Me.PositronDivider1.Colors = New Warcraft_StateCheck.Bloom(-1) {}
+        Me.PositronDivider1.Customization = ""
+        Me.PositronDivider1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.PositronDivider1.Image = Nothing
+        Me.PositronDivider1.Location = New System.Drawing.Point(3, 64)
+        Me.PositronDivider1.Name = "PositronDivider1"
+        Me.PositronDivider1.NoRounding = False
+        Me.PositronDivider1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        Me.PositronDivider1.Size = New System.Drawing.Size(275, 14)
+        Me.PositronDivider1.TabIndex = 10
+        Me.PositronDivider1.Text = "PositronDivider1"
+        Me.PositronDivider1.Transparent = True
+        '
+        'btn_Enable
+        '
+        Me.btn_Enable.Customization = "3Nzc/8jIyP9kZGT/lpaW/8jIyP/S0tL/"
+        Me.btn_Enable.Font = New System.Drawing.Font("Verdana", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Enable.Image = Nothing
+        Me.btn_Enable.Location = New System.Drawing.Point(54, 32)
+        Me.btn_Enable.Name = "btn_Enable"
+        Me.btn_Enable.NoRounding = False
+        Me.btn_Enable.Size = New System.Drawing.Size(177, 30)
+        Me.btn_Enable.TabIndex = 9
+        Me.btn_Enable.Text = "Enable state check"
+        Me.btn_Enable.Transparent = False
+        '
+        'btn_Minimize
+        '
+        Me.btn_Minimize.Customization = "3Nzc/8jIyP9kZGT/lpaW/8jIyP/S0tL/"
+        Me.btn_Minimize.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Minimize.Image = Nothing
+        Me.btn_Minimize.Location = New System.Drawing.Point(245, 3)
+        Me.btn_Minimize.Name = "btn_Minimize"
+        Me.btn_Minimize.NoRounding = False
+        Me.btn_Minimize.Size = New System.Drawing.Size(20, 20)
+        Me.btn_Minimize.TabIndex = 8
+        Me.btn_Minimize.Text = "_"
+        Me.btn_Minimize.Transparent = False
+        '
+        'btn_Close
+        '
+        Me.btn_Close.Customization = "3Nzc/8jIyP9kZGT/lpaW/8jIyP/S0tL/"
+        Me.btn_Close.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_Close.Image = Nothing
+        Me.btn_Close.Location = New System.Drawing.Point(267, 3)
+        Me.btn_Close.Name = "btn_Close"
+        Me.btn_Close.NoRounding = False
+        Me.btn_Close.Size = New System.Drawing.Size(20, 20)
+        Me.btn_Close.TabIndex = 7
+        Me.btn_Close.Text = "X"
+        Me.btn_Close.Transparent = False
+        '
+        'lb_State
+        '
+        Me.lb_State.AutoSize = True
+        Me.lb_State.BackColor = System.Drawing.Color.Transparent
+        Me.lb_State.Font = New System.Drawing.Font("Verdana", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.lb_State.ForeColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.lb_State.Location = New System.Drawing.Point(69, 137)
+        Me.lb_State.Name = "lb_State"
+        Me.lb_State.Size = New System.Drawing.Size(58, 13)
+        Me.lb_State.TabIndex = 6
+        Me.lb_State.Text = "Unknow"
+        '
+        'lb_Status
+        '
+        Me.lb_Status.AutoSize = True
+        Me.lb_Status.BackColor = System.Drawing.Color.Transparent
+        Me.lb_Status.Font = New System.Drawing.Font("Verdana", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.lb_Status.ForeColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.lb_Status.Location = New System.Drawing.Point(15, 137)
+        Me.lb_Status.Name = "lb_Status"
+        Me.lb_Status.Size = New System.Drawing.Size(52, 13)
+        Me.lb_Status.TabIndex = 5
+        Me.lb_Status.Text = "Status:"
+        '
+        'rdb_Soundplayer
+        '
+        Me.rdb_Soundplayer.Checked = False
+        Me.rdb_Soundplayer.Customization = "ZGRk/6+vr//IyMj/lpaW/w=="
+        Me.rdb_Soundplayer.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.rdb_Soundplayer.Image = Nothing
+        Me.rdb_Soundplayer.Location = New System.Drawing.Point(95, 112)
+        Me.rdb_Soundplayer.Name = "rdb_Soundplayer"
+        Me.rdb_Soundplayer.NoRounding = False
+        Me.rdb_Soundplayer.Size = New System.Drawing.Size(111, 22)
+        Me.rdb_Soundplayer.TabIndex = 4
+        Me.rdb_Soundplayer.Text = "Sound player"
+        Me.rdb_Soundplayer.Transparent = False
+        '
+        'rdb_Msgbox
+        '
+        Me.rdb_Msgbox.Checked = False
+        Me.rdb_Msgbox.Customization = "ZGRk/6+vr//IyMj/lpaW/w=="
+        Me.rdb_Msgbox.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.rdb_Msgbox.Image = Nothing
+        Me.rdb_Msgbox.Location = New System.Drawing.Point(140, 84)
+        Me.rdb_Msgbox.Name = "rdb_Msgbox"
+        Me.rdb_Msgbox.NoRounding = False
+        Me.rdb_Msgbox.Size = New System.Drawing.Size(102, 22)
+        Me.rdb_Msgbox.TabIndex = 3
+        Me.rdb_Msgbox.Text = "Messagebox"
+        Me.rdb_Msgbox.Transparent = False
+        '
+        'rdb_Autofocus
+        '
+        Me.rdb_Autofocus.Checked = True
+        Me.rdb_Autofocus.Customization = "ZGRk/6+vr//IyMj/lpaW/w=="
+        Me.rdb_Autofocus.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.rdb_Autofocus.Image = Nothing
+        Me.rdb_Autofocus.Location = New System.Drawing.Point(46, 84)
+        Me.rdb_Autofocus.Name = "rdb_Autofocus"
+        Me.rdb_Autofocus.NoRounding = False
+        Me.rdb_Autofocus.Size = New System.Drawing.Size(88, 22)
+        Me.rdb_Autofocus.TabIndex = 2
+        Me.rdb_Autofocus.Text = "Auto focus"
+        Me.rdb_Autofocus.Transparent = False
+        '
+        'frm_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(290, 164)
+        Me.Controls.Add(Me.ThemeMain)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frm_Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Warcraft State Check"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.ThemeMain.ResumeLayout(False)
+        Me.ThemeMain.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ThemeMain As Warcraft_StateCheck.PositronTheme
+    Friend WithEvents rdb_Soundplayer As Warcraft_StateCheck.PositronRadioButton
+    Friend WithEvents rdb_Msgbox As Warcraft_StateCheck.PositronRadioButton
+    Friend WithEvents rdb_Autofocus As Warcraft_StateCheck.PositronRadioButton
+    Friend WithEvents ToolTip_Info As System.Windows.Forms.ToolTip
+    Friend WithEvents lb_State As Warcraft_StateCheck.PositronLabel
+    Friend WithEvents lb_Status As Warcraft_StateCheck.PositronLabel
+    Friend WithEvents btn_Minimize As Warcraft_StateCheck.PositronButton
+    Friend WithEvents btn_Close As Warcraft_StateCheck.PositronButton
+    Friend WithEvents PositronDivider1 As Warcraft_StateCheck.PositronDivider
+    Friend WithEvents btn_Enable As Warcraft_StateCheck.PositronButton
+    Friend WithEvents tmr_CheckAvalibleProcess As System.Windows.Forms.Timer
+    Friend WithEvents tmr_ReadMemory As System.Windows.Forms.Timer
+    Friend WithEvents tmr_ReadMemory2 As System.Windows.Forms.Timer
+    Friend WithEvents tmr_AddText As System.Windows.Forms.Timer
+End Class
---
+++ Warcraft StateCheck/frm_Main.vb
@@ -0,0 +1,289 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Threading
+
+Public Class frm_Main
+
+    Dim Radio_Checked As Integer = -1
+    Public _Memory As GFunction.Memory
+    <DllImport("user32.dll")> _
+    Private Shared Function SetForegroundWindow(ByVal hWnd As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+    Private Declare Auto Function IsIconic Lib "user32.dll" (ByVal hwnd As IntPtr) As Boolean
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> _
+    Private Shared Function ShowWindow(ByVal hwnd As IntPtr, ByVal nCmdShow As ShowWindowCommands) As Boolean
+    End Function
+    Enum ShowWindowCommands As Integer
+        ''' <summary>
+        ''' Hides the window and activates another window.
+        ''' </summary>
+        Hide = 0
+        ''' <summary>
+        ''' Activates and displays a window. If the window is minimized or
+        ''' maximized, the system restores it to its original size and position.
+        ''' An application should specify this flag when displaying the window
+        ''' for the first time.
+        ''' </summary>
+        Normal = 1
+        ''' <summary>
+        ''' Activates the window and displays it as a minimized window.
+        ''' </summary>
+        ShowMinimized = 2
+        ''' <summary>
+        ''' Maximizes the specified window.
+        ''' </summary>
+        Maximize = 3
+        ' is this the right value?
+        ''' <summary>
+        ''' Activates the window and displays it as a maximized window.
+        ''' </summary>      
+        ShowMaximized = 3
+        ''' <summary>
+        ''' Displays a window in its most recent size and position. This value
+        ''' is similar to <see cref="Win32.ShowWindowCommands.Normal"/>, except
+        ''' the window is not actived.
+        ''' </summary>
+        ShowNoActivate = 4
+        ''' <summary>
+        ''' Activates the window and displays it in its current size and position.
+        ''' </summary>
+        Show = 5
+        ''' <summary>
+        ''' Minimizes the specified window and activates the next top-level
+        ''' window in the Z order.
+        ''' </summary>
+        Minimize = 6
+        ''' <summary>
+        ''' Displays the window as a minimized window. This value is similar to
+        ''' <see cref="Win32.ShowWindowCommands.ShowMinimized"/>, except the
+        ''' window is not activated.
+        ''' </summary>
+        ShowMinNoActive = 7
+        ''' <summary>
+        ''' Displays the window in its current size and position. This value is
+        ''' similar to <see cref="Win32.ShowWindowCommands.Show"/>, except the
+        ''' window is not activated.
+        ''' </summary>
+        ShowNA = 8
+        ''' <summary>
+        ''' Activates and displays the window. If the window is minimized or
+        ''' maximized, the system restores it to its original size and position.
+        ''' An application should specify this flag when restoring a minimized window.
+        ''' </summary>
+        Restore = 9
+        ''' <summary>
+        ''' Sets the show state based on the SW_* value specified in the
+        ''' STARTUPINFO structure passed to the CreateProcess function by the
+        ''' program that started the application.
+        ''' </summary>
+        ShowDefault = 10
+        ''' <summary>
+        '''  <b>Windows 2000/XP:</b> Minimizes a window, even if the thread
+        ''' that owns the window is not responding. This flag should only be
+        ''' used when minimizing windows from a different thread.
+        ''' </summary>
+        ForceMinimize = 11
+    End Enum
+
+    Public Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Public Const HT_CAPTION As Integer = &H2
+
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> _
+    Private Shared Function SendMessage(ByVal hWnd As IntPtr,
+            ByVal Msg As UInteger,
+            ByVal wParam As IntPtr,
+            ByVal lParam As IntPtr) As IntPtr
+    End Function
+
+    <DllImportAttribute("user32.dll")> _
+    Public Shared Function ReleaseCapture() As Boolean
+    End Function
+
+
+    Private Sub rdb_Autofocus_MouseEnter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rdb_Autofocus.MouseEnter
+        ToolTip_Info.Show("Auto focus when Game started.", rdb_Autofocus)
+    End Sub
+    Private Sub rdb_Autofocus_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rdb_Autofocus.MouseLeave
+        ToolTip_Info.Hide(rdb_Autofocus)
+    End Sub
+    Private Sub ThemeMain_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles ThemeMain.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            ReleaseCapture()
+            'SendMessage(Me.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 1) '112 'F012
+            SendMessage(Me.Handle, &H112, &HF012, 1)
+        End If
+    End Sub
+
+
+    Private Sub rdb_Msgbox_MouseEnter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rdb_Msgbox.MouseEnter
+        ToolTip_Info.Show("Show messagebox when Game Started.", rdb_Msgbox)
+    End Sub
+
+    Private Sub rdb_Msgbox_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rdb_Msgbox.MouseLeave
+        ToolTip_Info.Hide(rdb_Msgbox)
+    End Sub
+
+    Private Sub rdb_Soundplayer_MouseEnter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rdb_Soundplayer.MouseEnter
+        ToolTip_Info.Show("Show messagebox when Game Started.", rdb_Soundplayer)
+    End Sub
+
+    Private Sub rdb_Soundplayer_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rdb_Soundplayer.MouseLeave
+        ToolTip_Info.Hide(rdb_Soundplayer)
+    End Sub
+
+    Private Sub btn_Close_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Close.Click
+        Me.Close()
+    End Sub
+
+    Private Sub bbtn_Minimize_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Minimize.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub btn_Enable_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_Enable.Click
+        If btn_Enable.Text.ToLower = "enable state check" Then
+            tmr_ReadMemory.Start()
+            btn_Enable.Text = "Disable state check"
+
+        Else
+            tmr_ReadMemory.Stop()
+            tmr_ReadMemory2.Stop()
+            btn_Enable.Text = "Enable state check"
+        End If
+    End Sub
+
+    Private Sub tmr_CheckAvalibleProcess_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmr_CheckAvalibleProcess.Tick
+        If Process.GetProcessesByName("Cybergames").Length > 0 Then
+            If Process.GetProcessesByName("war3").Length > 0 Then
+                btn_Enable.Enabled = True
+            Else
+                btn_Enable.Enabled = False
+            End If
+        Else : btn_Enable.Enabled = False
+        End If
+    End Sub
+
+    Private Sub frm_Main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Select Case My.Settings.radioSave
+            Case 1
+                rdb_Autofocus.Checked = True
+            Case 2
+                rdb_Msgbox.Checked = True
+            Case 3
+                rdb_Soundplayer.Checked = True
+        End Select
+        tmr_CheckAvalibleProcess.Start()
+    End Sub
+
+    Private Sub tmr_ReadMemory_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmr_ReadMemory.Tick
+        If Process.GetProcessesByName("Cybergames").Length > 0 Then
+            If Process.GetProcessesByName("war3").Length > 0 Then
+                _Memory.GetProcessHandle("war3")
+                Dim adr As Integer = (_Memory.GetBaseAddress("Game.dll") + &HACE638)
+                Dim value As Integer = _Memory.ReadInteger(adr)
+                Select Case value
+                    Case 229
+                        lb_State.Text = "Waiting host start game"
+                        tmr_ReadMemory2.Start()
+                        tmr_ReadMemory.Stop()
+                End Select
+            End If
+        End If
+
+    End Sub
+
+    Private Sub tmr_ReadMemory2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmr_ReadMemory2.Tick
+        If Process.GetProcessesByName("Cybergames").Length > 0 Then
+            If Process.GetProcessesByName("war3").Length > 0 Then
+                _Memory.GetProcessHandle("war3")
+                Dim adr As Integer = (_Memory.GetBaseAddress("Game.dll") + &HACE638)
+                Dim value As Integer = _Memory.ReadInteger(adr)
+                Select Case value
+                    Case 63
+                        tmr_ReadMemory2.Stop()
+                        tmr_ReadMemory.Start()
+                        tmr_AddText.Stop()
+                        lb_State.Text = "You were kicked"
+                        If Radio_Checked = 1 Then
+                            If IsIconic(Process.GetProcessesByName("war3")(0).MainWindowHandle) = True Then
+                                ShowWindow(Process.GetProcessesByName("war3")(0).MainWindowHandle, ShowWindowCommands.Restore)
+                                SetForegroundWindow(Process.GetProcessesByName("war3")(0).MainWindowHandle)
+                            Else
+                                SetForegroundWindow(Process.GetProcessesByName("war3")(0).MainWindowHandle)
+                            End If
+                          ElseIf Radio_Checked = 2 Then
+                            MessageBox.Show("You were kicked", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                        ElseIf Radio_Checked = 3 Then
+                            My.Computer.Audio.Play(My.Resources.You_were_kicked, AudioPlayMode.Background)
+                        End If
+                    Case 14
+                        tmr_AddText.Stop()
+                        lb_State.Text = "Loading map"
+                    Case 404
+                        tmr_ReadMemory2.Stop()
+                        tmr_ReadMemory.Start()
+                        tmr_AddText.Stop()
+                        lb_State.Text = "Game started"
+                        If Radio_Checked = 1 Then
+                            If IsIconic(Process.GetProcessesByName("war3")(0).MainWindowHandle) = True Then
+                                ShowWindow(Process.GetProcessesByName("war3")(0).MainWindowHandle, ShowWindowCommands.Restore)
+                                SetForegroundWindow(Process.GetProcessesByName("war3")(0).MainWindowHandle)
+                            Else
+                                SetForegroundWindow(Process.GetProcessesByName("war3")(0).MainWindowHandle)
+                            End If
+                        ElseIf Radio_Checked = 2 Then
+                            MessageBox.Show("Game started", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+                        ElseIf Radio_Checked = 3 Then
+                            My.Computer.Audio.Play(My.Resources.Game_Started, AudioPlayMode.Background)
+                        End If
+                End Select
+            End If
+        End If
+    End Sub
+
+
+    Private Sub lb_State_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lb_State.TextChanged
+        Select Case lb_State.Text
+            Case "Waiting host start game"
+                lb_State.ForeColor = Color.DarkGoldenrod
+                tmr_AddText.Start()
+            Case "Waiting host start game.........."
+                lb_State.Text = "Waiting host start game"
+            Case "You were kicked"
+                lb_State.ForeColor = Color.HotPink
+                tmr_AddText.Stop()
+            Case "Loading map"
+                lb_State.ForeColor = Color.Lime
+            Case "Loading map..................."
+                lb_State.Text = "Loading map"
+                lb_State.ForeColor = Color.Lime
+            Case "Game started"
+                lb_State.ForeColor = Color.LimeGreen
+        End Select
+    End Sub
+
+    Private Sub tmr_AddText_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmr_AddText.Tick
+        lb_State.Text += "."
+    End Sub
+
+    Private Sub rdb_Autofocus_CheckedChanged(ByVal sender As System.Object) Handles rdb_Autofocus.CheckedChanged
+        If rdb_Autofocus.Checked = True Then
+            Radio_Checked = 1
+        End If
+    End Sub
+
+    Private Sub rdb_Msgbox_CheckedChanged(ByVal sender As System.Object) Handles rdb_Msgbox.CheckedChanged
+        If rdb_Msgbox.Checked = True Then
+            Radio_Checked = 2
+        End If
+    End Sub
+
+    Private Sub rdb_Soundplayer_CheckedChanged(ByVal sender As System.Object) Handles rdb_Soundplayer.CheckedChanged
+        If rdb_Soundplayer.Checked = True Then
+            Radio_Checked = 3
+        End If
+    End Sub
+
+    Private Sub frm_Main_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        My.Settings.radioSave = Radio_Checked
+    End Sub
+
+End Class
